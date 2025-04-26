8d3d03b9cc83bdcf1fe857a9ebc22b65c6fb10a3
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-04-26 21:12:37 +0300

    feat: create basic post structure for post item in list

---
+++ src/components/PostItem/PostItem.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from "react";
+import Button from "../UI/Button/Button";
+import styles from "./PostItem.module.scss";
+
+type TPostItemProps = {
+  post: TPost;
+  number: number;
+  remove: (post: TPost) => void;
+};
+
+export type TPost = {
+  id: string ;
+  title: string;
+  body: string;
+};
+
+
+const PostItem: FC<TPostItemProps> = (props: TPostItemProps) => {
+  const { post, number, remove } = props;
+  return (
+    <div className={styles.post}>
+      <div>
+        <strong>
+          {number}. {post.title}
+        </strong>
+        <div>{post.body}</div>
+      </div>
+      <div>
+        <Button className={styles.button__delete} onClick={()=>remove(post)}>
+          Удалить
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PostItem;
