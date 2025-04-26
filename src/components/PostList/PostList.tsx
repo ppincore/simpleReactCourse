9d19f7b085f67c78f98bd532528ca8a689afafeb
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-04-26 21:13:02 +0300

    feat: add post list component, for view post items in app

---
+++ src/components/PostList/PostList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import PostItem from "../PostItem/PostItem";
+import { TPost } from "../PostItem/PostItem";
+import styles from './PostList.module.scss'
+type TPostListProps = {
+  posts?: TPost[];
+  remove: (post: TPost) => void;
+  title: string;
+};
+
+const PostList = (props: TPostListProps) => {
+  const { posts, remove, title } = props;
+  if (!posts.length) {
+    return (
+      <>
+        <h1 className={styles.title}>{`Список постов пуст :(`}</h1>
+      </>
+    );
+  }
+  return (
+    <div>
+      <h1 className={styles.title}>{title}</h1>
+      {posts.map((post, index) => (
+        <PostItem
+          remove={remove}
+          number={index + 1}
+          post={post}
+          key={post.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
