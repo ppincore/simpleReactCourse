fde73796c08864cee4d8f9f9465b07c21fda1d4a
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-05-11 22:09:44 +0300

    feat: add route for current post

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -3,10 +3,13 @@ import Posts from "../../pages/Posts/Posts";
 import NotFound from "../../pages/notFound/NotFound";
 import { Routes, Route } from "react-router-dom";
 import About from "../../pages/About/About";
+import PostPage from "../../pages/PostPage/PostPage";
+import Modal from "../Modal/Modal";
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/posts" element={<Posts />} />
+      <Route path = '/posts' element={<Posts/>}/>
+      <Route path="/posts/:id" element={<PostPage />}/>
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
