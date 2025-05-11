5c4f92614664dd3c9dfeecd5ecb8b11c62712ddc
Bogdan Gaevskij <liltracyn@yandex.ru>
2025-05-11 22:11:00 +0300

    feat: add fetch func for post by ID and comments to current post

--- src/api/appApi.ts
+++ src/api/appApi.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
-const URL = import.meta.env.VITE_POST_API_URL
+const URL = import.meta.env.VITE_POST_API_URL;
 
 export const fetchPosts = async (limit = 10, page = 1) => {
   const response = await axios.get(`${URL}/posts`, {
-    params:{
+    params: {
       _limit: limit,
-      _page: page
-    }
-  })
-  return response
-}
+      _page: page,
+    },
+  });
+  return response;
+};
+
+export const fetchPostById = async (id: string) => {
+  const response = await axios.get(`${URL}/posts/${id}`);
+  return response;
+};
+
+export const fetchCommentsById = async (id: string) => {
+  const response = await axios.get(`${URL}/coments/${id}`);
+  return response;
+};
--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchPostById } from "../../api/appApi";
+import { TPost } from "../../components/PostItem/PostItem";
+import { useFetch } from "../../hooks/useFetch";
+
+import Loader from "../../components/UI/Loader/Loader";
 
 const PostPage = () => {
+  const params = useParams();
+  const [post, setPost] = useState<TPost | null>(null);
+  const [comments, setComments] = useState()
+  const [fetchPost, isLoading, error] = useFetch(async () => {
+    const response = await fetchPostById(params.id);
+    setPost(response.data);
+  });
+  const [fetchComments, isLoadingComents, commentsError] = useFetch(async () => {
+    const response = await fetchPostById(params.id);
+    setPost(response.data);
+  });
+
+  useEffect(() => {
+    if (params.id) {
+      fetchPost();
+    } else{
+      throw Error(error)
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Opened POST</h1>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <div>
+            <div>ПОСТ {post?.id}</div>
+            <div>{post?.title}</div>
+            <div>{post?.body}</div>
+            <div></div>
+          </div>
+        </>
+      )}
+      <h1>Комментарии</h1>
+    </>
   );
 };
 
